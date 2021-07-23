ad0c7e8ca1e3cf2c66e61777f02e7766dd709958
siobhan <sr@saibhreas.com>
2021-07-22 23:08:58 -0400

    tired, playing mwith the header and relative position

--- style-test-721/src/App.js
+++ style-test-721/src/App.js
@@ -6,9 +6,8 @@ import './default.scss';
 function App() {
   return (
     <div className="App">
-      <h2>I happen first</h2>
       <Header>
-        <h3>I happen second</h3>
+        
       </Header> 
       <h3>this should be below the header </h3>
     </div>
--- style-test-721/src/components/Header/index.js
+++ style-test-721/src/components/Header/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles.scss';
 
+import Logo from './../../assets/SabLogo.png'
+
 const Header = props => {
   return (
    <header className="header">
-
+     <div className="wrap">
+       <div className="logo">
+        <img src={Logo} alt="Saibhreas Logo" />
+       </div>
+     </div>
    </header>
   );
 };
